cd5a3d4e78862cf36c45b76858892f31d06150d3
Marc Lavergne <mlavergn@gmail.com>
2020-01-10 22:40:53 -0500

    Add detailed send and capture logs

--- README.md
+++ README.md
@@ -1,19 +1,7 @@
-# IE XMLHttpRequest bug
-
-This project is a proof of concept for and existing IE11 XMLHttpRequest responseText bug.
-
-The project contains basic javascript XHR client code, and a simple HTTP server designed to fill the XMLHttpRequest channel with large amounts of text data.
-
-```text
+# IE XMLHttpRequest bugThis project is a proof of concept for and existing IE11 XMLHttpRequest responseText bug.The project contains basic javascript XHR client code, and a simple HTTP server designed to fill the XMLHttpRequest channel with large amounts of text data.```text
 IE11 XHR.responseText bug
---
-
-In IE11, when an asynchronous XMLHttpRequest responseText content length exceeds an unknown threshold (~60MB), IE11 will throw the following exception:
-
-
-  ERROR Error: Not enough storage is available to complete this operation.
-
-  objectError = {
+--In IE11, when an asynchronous XMLHttpRequest responseText content length exceeds an unknown threshold (~60MB), IE11 will throw the following exception:
+  ERROR Error: Not enough storage is available to complete this operation.  objectError = {
     description: "Error: Not enough storage is available to complete this operation.",
     message: "Error: Not enough storage is available to complete this operation.",
     name: "Error",
@@ -21,21 +9,7 @@ In IE11, when an asynchronous XMLHttpRequest responseText content length exceeds
     stack: "Error: Not enough storage is available to complete this operation.",
     Symbol: rxSubscriber
   }
-
-
-This behavior does not confirm to W3C XHR specifications which make no provisions for exceptions in asynchronous XHR requests. Only synchronous XHR requests are permitted to throw exceptions:
-
-https://xhr.spec.whatwg.org
-
-## Affected Frameworks
-
-As a result, RxJS and Angular do not trap or bubble up the exception. The top level app does not get called back with an error, with no error handling, and no indication the XHR request cannot be read, the top level app remains unaware that the connection is no longer readable.
-
-Here is the specific problem line for Angular 6:
-
-https://github.com/angular/angular/blob/6.0.x/packages/common/http/src/xhr.ts#L274
-
-```typescript
+This behavior does not confirm to W3C XHR specifications which make no provisions for exceptions in asynchronous XHR requests. Only synchronous XHR requests are permitted to throw exceptions:https://xhr.spec.whatwg.org## Affected FrameworksAs a result, RxJS and Angular do not trap or bubble up the exception. The top level app does not get called back with an error, with no error handling, and no indication the XHR request cannot be read, the top level app remains unaware that the connection is no longer readable.Here is the specific problem line for Angular 6:https://github.com/angular/angular/blob/6.0.x/packages/common/http/src/xhr.ts#L274```typescript
 if (req.responseType === 'text' && !!xhr.responseText) {
 +  try {
      progressEvent.partialText = xhr.responseText;
@@ -50,12 +24,6 @@ if (req.responseType === 'text' && !!xhr.responseText) {
 +    observer.error(res);
 +  }
 }
-```
-
-## Reports
-
-```text
-https://stackoverflow.com/search?q=responseText+%22Not+enough+storage+is+available+to+complete+this+operation%22
-
-https://github.com/jquery/jquery/issues/3499
+```## Reports```text
+https://stackoverflow.com/search?q=responseText+%22Not+enough+storage+is+available+to+complete+this+operation%22https://github.com/jquery/jquery/issues/3499
 ```
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -17,10 +18,14 @@ type EventSourcePayload struct {
 	Data        string
 	Origin      string
 	Source      string
-	LastEventID int
+	LastEventID int64
 }
 
 func handlerStatic(resp http.ResponseWriter, req *http.Request) {
+	// fmt.Println("handlerStatic")
+	resp.Header().Set("Access-Control-Allow-Origin", "*")
+	resp.Header().Set("Content-Type", "text/html")
+	resp.Header().Set("Cache-Control", "no-cache")
 	resp.WriteHeader(http.StatusOK)
 
 	file, err := os.Open("index.html")
@@ -38,67 +43,142 @@ func handlerStatic(resp http.ResponseWriter, req *http.Request) {
 }
 
 func handlerData(resp http.ResponseWriter, req *http.Request) {
+	// fmt.Println("handlerData")
+	resp.Header().Set("Access-Control-Allow-Origin", "*")
+	resp.Header().Set("Content-Type", "text/event-stream")
+	resp.Header().Set("Cache-Control", "no-cache")
 	resp.WriteHeader(http.StatusOK)
 
 	flusher, _ := resp.(http.Flusher)
 
 	fmt.Println(req.URL.Query())
-	payloads, _ := strconv.Atoi(req.URL.Query().Get("payloads"))
-	pauseAt, _ := strconv.Atoi(req.URL.Query().Get("pauseAt"))
+	sendBytes, _ := strconv.Atoi(req.URL.Query().Get("sendBytes"))
+	pauseAfter, _ := strconv.Atoi(req.URL.Query().Get("pauseAfter"))
 
-	fmt.Printf("Sending %d payloads, pausing at %d\n", payloads, pauseAt)
+	fmt.Printf("Sending %d bytes, flushing every %d sends\n", sendBytes, pauseAfter)
 
 	payload := &EventSourcePayload{
 		Type:        "message",
 		Data:        "{\"content\": \"The quick brown fox jumped over the lazy dog.\"}",
 		Origin:      "localhost",
-		LastEventID: 0,
+		LastEventID: time.Now().UnixNano() / 1000000,
 		Source:      "iexhrbug",
 	}
 
+	// agent := parseAgent(req.UserAgent())
+
+	frame := 1
+	print(spinner[0])
+
 	bytesSent := 0
 	payloadsSent := 0
-	pause := pauseAt
-	for payloadsSent < payloads {
-		payload.LastEventID++
+	pause := pauseAfter
+	for bytesSent < sendBytes {
+		select {
+		case <-req.Context().Done():
+			return
+		default:
+			break
+		}
 
+		payload.LastEventID = time.Now().UnixNano() / 1000000
 		raw := fmt.Sprintf("type:%s\ndata:%s\norigin:%s\nlastEventId:%d\nsource:%s\n\n", payload.Type, payload.Data, payload.Origin, payload.LastEventID, payload.Source)
 		data := []byte(raw)
 
-		bytesSent += len(data)
+		payloadLen := len(data)
+		bytesSent += payloadLen
 		payloadsSent++
 
 		resp.Write(data)
+		time.Sleep(10 * time.Millisecond)
 		flusher.Flush()
+		updateSpinner(&frame)
 
-		pause--
-		if pause == 0 {
-			fmt.Printf("Payloads sent %d / bytes sent %d\n", payloadsSent, bytesSent)
-			time.Sleep(1 * time.Second)
-			pause = pauseAt
+		pause -= payloadLen
+		if pause <= 0 {
+			// fmt.Printf("Agent [%s] payloads sent %d / bytes sent %d\n", agent, payloadsSent, bytesSent)
+			time.Sleep(1000 * time.Millisecond)
+			pause = pauseAfter
 		}
 	}
+
+	fmt.Printf("Test complete, sent %d / bytes sent %d\n", payloadsSent, bytesSent)
 }
 
 // Report type
 type Report struct {
-	Payloads int `json:"payloads"`
-	Bytes    int `json:"bytes"`
-	Chunks   int `json:"chunks"`
+	Bytes       int `json:"bytes"`
+	Chunks      int `json:"chunks"`
+	PayloadTime int `json:"payloadTime"`
+	BrowserTime int `json:"browserTime"`
 }
 
 func handlerReport(resp http.ResponseWriter, req *http.Request) {
+	// fmt.Println("handlerReport")
 	resp.WriteHeader(http.StatusOK)
 
 	data, _ := ioutil.ReadAll(req.Body)
 	defer req.Body.Close()
 
-	fmt.Println(string(data))
 	var report Report
 	json.Unmarshal(data, &report)
-	fmt.Println("Report: ", report)
+	agent := parseAgent(req.UserAgent())
+	fmt.Println("Agent: ", agent, " | Buffer: ", report.Bytes, " bytes | Lag: ", report.BrowserTime-report.PayloadTime, " ms")
 
 	resp.Write([]byte("OK"))
+}
+
+// Bug type
+type Bug struct {
+	Error       string `json:"error"`
+	Bytes       int    `json:"bytes"`
+	BrowserTime int    `json:"browserTime"`
+}
+
+func handlerBug(resp http.ResponseWriter, req *http.Request) {
+	// fmt.Println("handlerReport")
+	resp.WriteHeader(http.StatusOK)
+
+	data, _ := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+
+	var bug Bug
+	json.Unmarshal(data, &bug)
+	agent := parseAgent(req.UserAgent())
+	fmt.Println("!!!EXCEPTION!!! Agent: ", agent, " | Error: ", bug.Error, "Buffer: ", bug.Bytes, " bytes | Time: ", bug.BrowserTime, " ms")
+
+	resp.Write([]byte("OK"))
+}
+
+func parseAgent(userAgent string) string {
+	moz13 := userAgent[13:]
+	if strings.Contains(moz13, "Trident/") {
+		return "IE"
+	}
+	if strings.Contains(moz13, "Edge/") {
+		return "Edge"
+	}
+	if strings.Contains(moz13, "Chrome/") {
+		return "Chrome"
+	}
+	if strings.Contains(moz13, "Safari/") {
+		return "Safari"
+	}
+	return moz13[strings.LastIndex(moz13, " ")+1:]
+}
+
+// var spinnerX = []string{"⢀⠀", "⡀⠀", "⠄⠀", "⢂⠀", "⡂⠀", "⠅⠀", "⢃⠀", "⡃⠀", "⠍⠀", "⢋⠀", "⡋⠀", "⠍⠁", "⢋⠁", "⡋⠁", "⠍⠉", "⠋⠉", "⠋⠉", "⠉⠙", "⠉⠙", "⠉⠩", "⠈⢙", "⠈⡙", "⢈⠩", "⡀⢙", "⠄⡙", "⢂⠩", "⡂⢘", "⠅⡘", "⢃⠨", "⡃⢐", "⠍⡐", "⢋⠠", "⡋⢀", "⠍⡁", "⢋⠁", "⡋⠁", "⠍⠉", "⠋⠉", "⠋⠉", "⠉⠙", "⠉⠙", "⠉⠩", "⠈⢙", "⠈⡙", "⠈⠩", "⠀⢙", "⠀⡙", "⠀⠩", "⠀⢘", "⠀⡘", "⠀⠨", "⠀⢐", "⠀⡐", "⠀⠠", "⠀⢀", "⠀⡀"}
+
+// var spinner = []string{"🕛", "🕐", "🕑", "🕒", "🕓", "🕔", "🕕", "🕖", "🕗", "🕘", "🕙", "🕚"}
+
+var spinner = []string{"◒", "◑", "◓", "◐"}
+
+func updateSpinner(frame *int) {
+	print("\b\b", spinner[*frame])
+	*frame++
+	if *frame >= len(spinner) {
+		*frame = 0
+	}
 }
 
 func main() {
@@ -107,6 +187,7 @@ func main() {
 	http.Handle("/", http.HandlerFunc(handlerStatic))
 	http.Handle("/events", http.HandlerFunc(handlerData))
 	http.Handle("/report", http.HandlerFunc(handlerReport))
+	http.Handle("/bug", http.HandlerFunc(handlerBug))
 
 	// Start the server and listen forever on port 8000.
 	http.ListenAndServe(":8000", nil)
